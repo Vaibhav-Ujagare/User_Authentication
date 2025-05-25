cfc6d82b5d376d065e19d89389a0ed1c9b39aebe
Vaibhav-Ujagare <vaibhavujagare77@gmail.com>
2025-05-25 19:24:15 +0530

    added utils folder for standard api error response and handling async method

--- src/app.js
+++ src/app.js
@@ -1,5 +1,26 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(
+  cors({
+    origin: [
+      process.env.BASE_URL,
+      "http://127.0.0.1:5173",
+      "http://localhost:5173/signup",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PATCH", "OPTIONS", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+    exposedHeaders: ["Set-Cookie", "*"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static("public"));
 
 export default app;
